5e92fe44311b8d5dc0b822b87ad37af8da13ecb8
onoriode umukoro <onoriodeumukoro8@gmail.com>
2022-11-11 13:43:52 +0000

    added more transaction lists

--- src/containers/Accounts/Details/Transaction.js
+++ src/containers/Accounts/Details/Transaction.js
@@ -8,20 +8,30 @@ const transaction = () => (
             borderRadius: '5px',
             margin: '20px 0px'
         }}>
-        <div style={{margin: '15px 0px', fontSize: '1.4em'}}><strong>Transactions</strong></div>
+        <div style={{margin: '15px 15px', fontSize: '1em'}}><strong>Transactions</strong></div>
         <div style={{display: 'flex', justifyContent: "flex-end", marginTop: '-34px'}}>
             <div></div>
             <div style={{margin: '0px 2em'}}><AiOutlineSearch /></div>
             <div><strong>...</strong></div>
         </div>
-        <div style={{display: "flex", justifyContent: "space-between", margin: '18px 0px'}}>
-          <div><strong>TRANSF TO SAVINGS CONFIRMATIO...</strong></div>
-          <div style={{fontSize: '1.5em'}}>$5,000.00</div>
+        <div style={{display: "flex", justifyContent: "space-between", margin: '20px 15px'}}>
+          <div style={{fontSize: '13px'}}>TRANSF FROM (BP) COMPANY...</div>
+          <div style={{color: 'green', fontSize: '1em'}}>+$1,000,000.00</div>
         </div>
         <hr />
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-          <div><strong>TRSF FROM CHECKING ACCOUNT 1...</strong></div>
-          <div style={{color: 'green', fontSize: '1.5em'}}>+$5,000.00</div>
+        <div style={{display: "flex", justifyContent: "space-between",margin: '20px 15px'}}>
+          <div style={{fontSize: '13px'}}>TRANSF TO KELLY TOM...</div>
+          <div style={{color: 'green', fontSize: '1em'}}>$500.00</div>
+        </div>
+        <hr />
+        <div style={{display: "flex", justifyContent: "space-between", margin: '18px 15px'}}>
+          <div style={{fontSize: '13px'}}>TRANSF TO SAVINGS CONFIRMATIO...</div>
+          <div style={{fontSize: '1em'}}>$5,000.00</div>
+        </div>
+        <hr />
+        <div style={{display: "flex", justifyContent: "space-between", margin: '20px 15px'}}>
+          <div style={{fontSize: '13px'}}>TRSF FROM CHECKING ACCOUNT 1...</div>
+          <div style={{color: 'green', fontSize: '1em'}}>+$5,000.00</div>
         </div>
         </div>
     </header>
